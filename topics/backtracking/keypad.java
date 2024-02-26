1acfa520d4ddeb1417cc8b57ad9f0b73c9d91f6e
TheParas <paras2004jain@gmail.com>
2024-02-26 17:45:46 +0530

    sq

--- topics/backtracking/keypad.java
+++ topics/backtracking/keypad.java
@@ -1,4 +1,20 @@
 public class keypad {
     final char[][] K = {{},{},{'a','b','c'},{'d','e','f'},{'g','h','i'},{'j','k','l'},{'m','n','o'},
                         {'p','q','r','s'},{'t','u','v'},{'w','x','y','z'}};
+
+    public static void letterComb (String A) {
+        int len = A.length();
+        if(len == 0) {
+            System.out.println("");
+            return;
+        }
+
+        bfs(0, len, new StringBuilder(), A);
+    }
+
+    public static void bfs(int pos, int len, StringBuilder sb, String A) {
+        if(pos == len) {
+            System.out.println(sb.toString());
+        }
+    }
 }
