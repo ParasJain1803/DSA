961bf1a8f6965df8c3de8139e611142f82e4f63f
TheParas <paras2004jain@gmail.com>
2024-08-21 22:33:18 +0530

    formatting

--- topics/sorting/divideConquor.java
+++ topics/sorting/divideConquor.java
@@ -11,20 +11,20 @@ public class divideConquor {
             return;
         }
         int mid = si + (ei-si)/2;
-        mergeSort(arr,si,mid);                   //left part
-        mergeSort(arr,mid+1,ei);                //right part
-        merger(arr,si,mid,ei);                  //merging them
+        mergeSort(arr,si,mid);                  // left part
+        mergeSort(arr,mid+1,ei);                // right part
+        merger(arr,si,mid,ei);                  // merging them
     }
 
     public static void merger(int arr[], int si, int mid, int ei) {
         int temp[] = new int[ei-si+1];
-        int i = si;                     //iterator for left part
-        int j = mid+1;                  //iterator for right part
-        int k=0;                        //iterator for temp array
+        int i = si;                             // iterator for left part
+        int j = mid+1;                          // iterator for right part
+        int k=0;                                // iterator for temp array
 
-        while(i<=mid && j<=ei) {        //compairing and 
-            if(arr[i]<arr[j]) {         //putting them
-                temp[k] = arr[i];       //in temp array
+        while(i<=mid && j<=ei) {                // compairing and 
+            if(arr[i]<arr[j]) {                 // putting them
+                temp[k] = arr[i];               // in temp array
                 i++;
             }
             else {
@@ -34,13 +34,13 @@ public class divideConquor {
             k++;
         }
         while(i<=mid) {
-            temp[k++]=arr[i++];         //remaining left part
+            temp[k++]=arr[i++];                 // remaining left part
         }
         while(j<=ei) {
-            temp[k++]=arr[j++];         //remaining right part
+            temp[k++]=arr[j++];                 // remaining right part
         }
         for(k=0, i=si; k<temp.length; k++,i++) {
-            arr[i] = temp[k];           //copying to orignal array 
+            arr[i] = temp[k];                   //copying to orignal array 
         }
     }
     public static void main(String[] args) {
