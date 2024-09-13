f62ee9950ba6581b663c349dbacde6421fcfbd06
TheParas <paras2004jain@gmail.com>
2024-09-13 21:44:00 +0530

    stacks

---
+++ topics/stacks/stackLinkedList.java
@@ -0,0 +1,60 @@
+public class stackLinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    static class Stack {
+        static Node head = null;
+
+        public static boolean isEmpty() {
+            return head == null;
+        }
+
+        public static void push(int data) {
+            Node newNode = new Node(data);
+
+            if (isEmpty()) {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head = newNode;
+        }
+
+        public static int pop() {
+            if(isEmpty()) {
+                return -1;
+            }
+
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        public static int peek() {
+            if(isEmpty()) {
+                return -1;
+            }
+
+            int top = head.data;
+            return top;
+        }
+    }
+
+    public static void main(String args[]) { // Create an instance of the outer class
+            Stack s = new Stack();  // Create an instance of the inner class
+            s.push(0);
+            s.push(1);
+            s.push(2);
+            while (!s.isEmpty()) {
+                System.out.println(s.peek());
+                s.pop();
+            }
+    }
+}
---
+++ topics/stacks/stackUsingJCF.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class stackUsingJCF {
+    public static void main(String args[]) { 
+            Stack<Integer> s = new Stack<>();
+            s.push(0);
+            s.push(1);
+            s.push(2);
+            while (!s.isEmpty()) { 
+                System.out.println(s.peek());
+                s.pop();
+            }
+    }
+}
